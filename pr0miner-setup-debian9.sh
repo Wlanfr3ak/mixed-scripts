ab6bc8d2793cf4468550f89e366dc52f7a8d1463
Fabian Horst <fabian@fabian-horst.de>
2017-07-29 09:23:15 +0200

    Basic Installation Script Pr0 Mining at Debian9

---
+++ pr0miner-setup-debian9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Based on: https://github.com/holzmaster/node-pr0gramm-miner
+# With Code from: http://linuxbsdos.com/2017/06/26/how-to-install-node-js-lts-on-debian-9-stretch/
+# 
+# Howto:
+# Use "chmod +x pr0miner-setup-debian9.sh" to make it executable and start it with "./pr0miner-setup-debian9.sh"
+# After the Start the Script directly starts mining
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+apt-get install htop curl sudo git
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+sudo apt install nodejs
+node --version
+git clone https://github.com/holzmaster/node-pr0gramm-miner
+cd node-pr0gramm-miner
+npm i
+
+##
+# Add your personal Data here, you can find it under: https://pr0gramm.com/api/user/minerauth
+# 
+./miner.js -u <pr0username> -t <tokenid> -j <threadanzahl> -a -v
