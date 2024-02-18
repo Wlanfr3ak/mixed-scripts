2f05ded94abf2179cf01b9d0aee9327a403d43ab
Fabian Horst <6292882+Wlanfr3ak@users.noreply.github.com>
2024-02-18 09:57:04 +0100

    Update yuneec-ffmpeg.sh
    added new line for another option to render the video

--- yuneec-ffmpeg.sh
+++ yuneec-ffmpeg.sh
@@ -8,3 +8,7 @@ for i in *.mp4;
   ffmpeg -i "$i" -y -vcodec libx264  -movflags faststart -map 0:v:0 -pix_fmt yuv420p "${name}-normal.mp4"
   ffmpeg -i "$i" -y -vcodec libx264  -movflags faststart -map 0:v:1 -pix_fmt yuv420p "${name}-thermal.mp4"
 done
+
+# Special: Thermal Video over Normal Video:
+# ffmpeg -i YUN_0201.mp4 -filter_complex "[0:v:0]setpts=PTS-STARTPTS, scale=1920x1080[untere]; [0:v:1]setpts=PTS-STARTPTS, scale=800x600[obere]; [untere][obere]overlay=x=(main_w-overlay_w)/2:y=(main_h-overlay_h)/2" -c:v h264_videotoolbox output_video-appple.mp4
+# ffmpeg -i YUN_0201.mp4 -filter_complex "[0:v:0]setpts=PTS-STARTPTS, scale=1920x1080[untere]; [0:v:1]setpts=PTS-STARTPTS, scale=800x600[obere]; [untere][obere]overlay=x=(main_w-overlay_w)/2:y=(main_h-overlay_h)/2" -c:v libx264 output_video-appple.mp4
